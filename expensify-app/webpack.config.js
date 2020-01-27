0a6a72c0e244141b7d226b38e14f7e2453099f3c
Ryan Horstman <ryanmhorstman@gmail.com>
2020-01-27 14:42:53 -0600

    created boilerplate app, started expensify-app and added react-router to expensify app

---
+++ expensify-app/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            // only when files meet the criteria below! Run babel through ANY JS file.
+            test: /\.js$/,
+            // excludes anything in the node_modules folder.
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        historyApiFallback: true
+    }
+};
+
